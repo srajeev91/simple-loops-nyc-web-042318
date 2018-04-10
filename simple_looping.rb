729e33667ebbe4757aba773dde33478f8bdc1bf8
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-10 00:18:42 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
   loop do
     count = 0
     puts "#{phrase}"
-    break if count == number_of_times
+    break if count >= number_of_times
     count +=1
   end
   
